dcf81ba896e3ab6c5cc8562679639e6d6ddbcd1d
Marek <tomas.marek@kajot.cz>
2023-03-10 16:44:20 +0100

    50:50 tahak functionality

--- src/stores/topics.js
+++ src/stores/topics.js
@@ -4,47 +4,47 @@ export const useTopicsStore = defineStore('topics',{
   state:()=>({
     topics:[
       {id: 0, name: 'Musicaly fit', parentalTopic:'Music', questions:[
-        {id: 0,name: 'funk',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 1,name: 'pop', isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 2,name: 'jazz',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 0,name: 'funk',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 1,name: 'pop', isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 2,name: 'jazz',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
       ]},
       {id: 1, name: 'Mooovies ',parentalTopic:'Cinema', questions:[
-        {id: 3,name: 'horror',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 4,name: 'bromance',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 5,name: 'comedy',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 3,name: 'horror',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 4,name: 'bromance',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 5,name: 'comedy',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
       ]},
       {id: 2, name: 'Oscars',parentalTopic:'Cinema', questions:[
-        {id: 6,name: 'actor',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-        {id: 7,name: 'actress',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-        {id: 8,name: 'unknown',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
+        {id: 6,name: 'actor',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true},wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+        {id: 7,name: 'actress',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true},wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+        {id: 8,name: 'unknown',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true},wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
       ]},
       {id: 3, name: 'Animated Masterpieces',parentalTopic:'Cinema', questions:[
-        {id: 9,name: 'Shrek',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 10,name: 'Cars',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 11,name: 'Tangled',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 9,name: 'Shrek',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 10,name: 'Cars',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 11,name: 'Tangled',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
       ]},
       {id: 4, name: 'EU states',parentalTopic:'Geography', questions:[
-        {id: 12,name: 'Germany',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 13,name: 'Poland',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 14,name: 'Slovakia',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 12,name: 'Germany',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 13,name: 'Poland',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 14,name: 'Slovakia',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
       ]},
       {id: 5,name: 'NA states',parentalTopic:'Geography', questions:[
-        {id: 15,name: 'Alaska',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 16,name: 'Colorado',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
-        {id: 17,name: 'California',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 15,name: 'Alaska',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 16,name: 'Colorado',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
+        {id: 17,name: 'California',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes',active: true}, wrongA:[{id:'w0',answer: 'no'},{id:'w1',answer: 'no'},{id:'w2',answer: 'no'}]},
       ]},
     ],
     activeQuestion:{},
     activeTopics:[     
       {id: 4, name: 'EU states',parentalTopic:'Geography', questions:[
-      {id: 12,name: 'Germany',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-      {id: 13,name: 'Poland',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-      {id: 14,name: 'Slovakia',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
+      {id: 12,name: 'Germany',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+      {id: 13,name: 'Poland',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+      {id: 14,name: 'Slovakia',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
     ]},
     {id: 5,name: 'NA states',parentalTopic:'Geography', questions:[
-      {id: 15,name: 'Alaska',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-      {id: 16,name: 'Colorado',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
-      {id: 17,name: 'California',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo'},{id:'w1',answer: 'nooooo'},{id:'w2',answer: 'nn'}]},
+      {id: 15,name: 'Alaska',isActive: true, points: 500, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+      {id: 16,name: 'Colorado',isActive: true, points: 300, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
+      {id: 17,name: 'California',isActive: true, points: 100, rightA: {id: 'r0', answer:'yes'}, wrongA:[{id:'w0',answer: 'noo',active: true},{id:'w1',answer: 'nooooo',active: true},{id:'w2',answer: 'nn',active: true}]},
     ]},],
     parentalTopics:[],
     maxTopics: 5,
@@ -71,16 +71,18 @@ export const useTopicsStore = defineStore('topics',{
       tempArr.splice(Math.floor(Math.random()*4),0, this.activeQuestion?.rightA);
       return tempArr;
     },
-    getFiftyFifty(){
-      let tempArr=[...this.getActiveQuestionAnswersRan];
-      let rightIndex= tempArr.indexOf(tempArr.find(item=> item.id=='r0')) ;
+    getRightAnswer(){
+      return this.getActiveQuestionAnswersRan.find(item=> item.id=='r0');
+    },
+    getWrongAnswer(){
+      let rightIndex= this.getActiveQuestionAnswersRan.indexOf(this.getRightAnswer);
       let wrongIndex= Math.floor(Math.random()*4);
       while(wrongIndex == rightIndex){
         wrongIndex= Math.floor(Math.random()*4);
       };
-      tempArr.forEach(answer=> tempArr.indexOf(answer) != rightIndex && tempArr.indexOf(answer) != wrongIndex? answer.answer='': null);
-      return tempArr;
-    }
+      return this.getActiveQuestionAnswersRan.at(wrongIndex)
+    },
+
   },
   actions:{
     printParents(){
@@ -99,7 +101,20 @@ export const useTopicsStore = defineStore('topics',{
     disableTopic(topicId){
       let wantedTopic= this.topics.find(topic=> topic.id== topicId);
       this.activeTopics.splice(this.activeTopics.indexOf(wantedTopic),1)
-    }
+    },
+    toggleFiftyFifty(){
+      let temp=this.activeQuestion.wrongA ;
+
+      while( this.activeQuestion.wrongA.filter(item=> item.active == true).length > 1){
+      let x =Math.floor(Math.random()*3)
+      if(temp.at(x) != this.getWrongAnswer){
+        console.log('x', x)
+        this.activeQuestion.wrongA.at(x).active= false;
+      }
+
+      }
+    },
+
   }
   
 })
--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -10,19 +10,79 @@ export default {
   data() {
     return {};
   },
+  computed: {
+    activeQuestion() {
+      return this.topicsStore.getActiveQuestion;
+    },
+    allAnswers() {
+      let x = [...this.topicsStore.getActiveQuestionAnswersRan];
+      return x;
+    },
+  },
   methods: {},
 };
 </script>
 <template>
   <div class="page-content">
-    <h1>{{ topicsStore.getActiveQuestion.name }}</h1>
-    <div v-for="answer in topicsStore.getActiveQuestionAnswersRan">
-      {{ answer.answer }}
+    <h1>{{ activeQuestion.name }}</h1>
+    {{ activeQuestion.content }}
+    <div>
+      <img :src="activeQuestion.imageUrl" />
     </div>
-    <div v-for="answer in topicsStore.getFiftyFifty">
-      {{ answer }}
+    <div v-for="answer in allAnswers">
+      <div v-if="answer.active">
+        {{ answer.answer }}
+      </div>
+    </div>
+    <div id="options">
+      <h3 id="options-cap">Tahák</h3>
+      <button class="option-btn" @click="this.topicsStore.toggleFiftyFifty">
+        50:50
+      </button>
+      <button class="option-btn">pepege</button>
+      <button class="option-btn" disabled>sharing is caring</button>
     </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.page-content {
+  position: relative;
+}
+#options {
+  z-index: 50;
+  position: absolute;
+  top: 10px;
+  right: 30px;
+  width: 250px;
+  height: 250px;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  background-color: var(--yellow);
+}
+#options-cap {
+  padding-top: 10px;
+  text-align: center;
+  width: 100%;
+}
+.option-btn {
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  font-size: 1.2rem;
+}
+.option-btn:hover {
+  color: red;
+}
+.option-btn {
+  background-color: none;
+  border: none;
+}
+.option-btn:disabled {
+  color: black;
+  text-decoration: line-through;
+  text-decoration-color: red;
+  text-decoration-thickness: 3px;
+}
+</style>
